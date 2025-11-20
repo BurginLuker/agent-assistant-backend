d91ac8b5abb1fe868404c6440f3490890530e709
BurginLuker <burginluker@gmail.com>
2025-11-19 20:49:12 -0700

    add cron to make project not pause

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import multer from "multer";
 import cookieParser from "cookie-parser";
 import { getUser, verifyToken } from "./Middleware/auth.js";
 import generatedListings from "./Documents/GeneratedListings.js";
+import cron from 'node-cron';
 
 const app = express();
 const port = 8080; // You can choose any available port
@@ -11,6 +12,7 @@ const port = 8080; // You can choose any available port
 import propertyController from "./contollers/propertyController.js";
 import contentController from "./contollers/contentController.js";
 import feedbackController from "./contollers/feedbackController.js";
+import supabase from "./utils/supabaseClient.js";
 
 // Enable CORS for all routes
 app.use(cors());
@@ -25,6 +27,39 @@ app.get("/api/healthcheck", (req, res) => {
     status: "OK",
   });
 });
+
+
+cron.schedule('0 */8 * * *', async () => {
+  const tables = ['Content', 'Property', 'Feedback','PropertyFiles'];
+  // Select a random table from the array
+  const randomIndex = Math.floor(Math.random() * tables.length);
+  const randomTable = tables[randomIndex];
+  
+  // Query 1 row from the randomly selected table
+  const { data, error } = await supabase.from(randomTable).select().limit(1);
+  if (error) {
+    console.error(`Error fetching data from ${randomTable}:`, error);
+  } else {
+    console.log(`Data from ${randomTable}:`, data);
+  }
+});
+
+// Cron job to run every 1 second
+cron.schedule('*/1 * * * * *', async () => {
+  const tables = ['Content', 'Property', 'Feedback','PropertyFiles'];
+  // Select a random table from the array
+  const randomIndex = Math.floor(Math.random() * tables.length);
+  const randomTable = tables[randomIndex];
+  
+  // Query 1 row from the randomly selected table
+  const { data, error } = await supabase.from(randomTable).select().limit(1);
+  if (error) {
+    console.error(`Error fetching data from ${randomTable}:`, error);
+  } else {
+    console.log(`Data from ${randomTable}:`, data);
+  }
+});
+
 
 app.use("/api/property", propertyController);
 app.use("/api/content", contentController);
